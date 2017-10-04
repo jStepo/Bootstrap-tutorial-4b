e4caac4fcb2b8db2e59aadac74d23aeb6a45b78b
jstep <jstep@TPL>
2017-10-04 13:43:56 +0200

    Kontakt Final

---
+++ MyShit/uben.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 51.208328, lng: 6.821417};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }

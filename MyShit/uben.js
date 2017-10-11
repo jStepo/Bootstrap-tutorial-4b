fc5bd47a4f01a484027678f80d539e68c4a0e913
jstep <jstep@TPL>
2017-10-11 14:26:35 +0200

    Test

--- MyShit/uben.js
+++ MyShit/uben.js
@@ -1,3 +1,4 @@
+//GoogleMaps API
 function initMap() {
         var uluru = {lat: 51.208328, lng: 6.821417};
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -9,3 +10,24 @@ function initMap() {
           map: map
         });
       }
+//Üben Auswahl ein und ausblenden
+var option = document.querySelectorAll("option");
+var fieldset = document.querySelectorAll("fieldset");
+
+option[0].addEventListener("click",function(){
+  fieldset[0].classList.add("ausblenden");
+  fieldset[1].classList.add("ausblenden");
+});
+option[1].addEventListener("click",function(){
+  fieldset[0].classList.remove("ausblenden");
+});
+option[2].addEventListener("click",function(){
+  fieldset[1].classList.add("ausblenden");
+});
+option[4].addEventListener("click",function(){
+  fieldset[1].classList.remove("ausblenden");
+});
+//Platzhalter für Game
+option[10].addEventListener("click",function(){
+  fieldset[1].classList.remove("ausblenden");
+});
